64cddc6022f8b73fb5e67b8dfab774922dbd9af8
btop <theqtkc@qq.com>
2020-10-11 22:11:41 +0800

    错误信息

--- util/context/context.go
+++ util/context/context.go
@@ -18,17 +18,22 @@ func (w *wrap) Success(data interface{}) {
 	return
 }
 
-func (w *wrap) FailErr(code int, err error) {
-	w.JSON(http.StatusOK, gin.H{
-		"code":   code,
+func (w *wrap) FailErr(err error) {
+
+	w.JSON(http.StatusNotAcceptable, gin.H{
+		"code":   10000,
 		"errors": util.GetErrors(err),
 	})
 	return
 }
 
-func (w *wrap) FailMsg(code int, msg string) {
-	w.JSON(http.StatusOK, gin.H{
-		"code": code,
+func (w *wrap) FailMsg(msg string, code ...int) {
+	statusCode := 10001
+	if len(code) == 1 {
+		statusCode = code[0]
+	}
+	w.JSON(http.StatusNotAcceptable, gin.H{
+		"code": statusCode,
 		"msg":  msg,
 	})
 	return
